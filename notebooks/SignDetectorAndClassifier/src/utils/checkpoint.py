78dfcba5073f3330a721fae7890c29c8191a7518
7491 Eug <macheev2010@yandex.ru>
2022-05-14 18:06:22 +0300

    Add capability to load single model

--- notebooks/SignDetectorAndClassifier/src/utils/checkpoint.py
+++ notebooks/SignDetectorAndClassifier/src/utils/checkpoint.py
@@ -8,7 +8,7 @@ def save_checkpoint(model, scheduler, optimizer, epoch, filename):
         'scheduler': scheduler.state_dict() if scheduler else None
     }, filename)
 
-def load_checkpoint(model, scheduler, optimizer, filename):
+def load_checkpoint(model, scheduler=None, optimizer=None, filename: str = 'model'):
     print(f'Loading checkpoint from {os.path.abspath(filename)}')
     checkpoint = torch.load(filename)
     if checkpoint['model']:
@@ -17,12 +17,12 @@ def load_checkpoint(model, scheduler, optimizer, filename):
     else:
         print('[!] Model was not loaded!')
 
-    if checkpoint['optimizer']:
+    if checkpoint['optimizer'] and not optimizer is None:
         optimizer.load_state_dict(checkpoint['optimizer'])
     else:
-        print('[!] optimizer was not loaded!')
+        print('[!] Optimizer was not loaded!')
 
-    if checkpoint['scheduler']:
+    if checkpoint['scheduler'] and not optimizer is None:
         scheduler.load_state_dict(checkpoint['scheduler'])
     else:
         print('[!] Scheduler was not loaded!')
