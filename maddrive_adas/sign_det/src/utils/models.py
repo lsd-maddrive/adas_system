99b675e919b26ac348240f8997dd6f7aae9dcd4e
GeneralFire <macheev2010@yandex.ru>
2022-06-12 15:05:40 +0300

    Format models

--- maddrive_adas/sign_det/src/utils/models.py
+++ maddrive_adas/sign_det/src/utils/models.py
@@ -2,11 +2,13 @@ import json
 import importlib
 from torch import nn
 
+
 def get_model_config(path_to_config: str):
     with open(path_to_config, 'rb') as f:
         config_data = f.read().decode('utf-8')
     model_data = json.loads(config_data)
     return model_data
+
 
 def get_model_and_img_size(path_to_config: str):
     model_data = get_model_config(path_to_config)
